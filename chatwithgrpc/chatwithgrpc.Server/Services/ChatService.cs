9ccbe16d689c37bb6f8aa958b0eeeefb23252063
Daniel Mrowca <daniel.mrowca@skkglobal.com>
2020-12-06 20:33:28 +0100

    Init

---
+++ chatwithgrpc/chatwithgrpc.Server/Services/ChatService.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Chat;
+using Grpc.Core;
+
+namespace chatwithgrpc
+{
+    public class ChatService: ChatRoom.ChatRoomBase
+    {
+        private readonly Server.ChatRoom _chatroomService;
+
+        public ChatService(Server.ChatRoom chatRoomService)
+        {
+            _chatroomService = chatRoomService;
+        }
+
+        public override async Task Join(IAsyncStreamReader<Message> requestStream, IServerStreamWriter<Message> responseStream, ServerCallContext context)
+        {
+            if (!await requestStream.MoveNext()) return;
+
+            do
+            {
+                _chatroomService.Join(requestStream.Current.User, responseStream);
+                await _chatroomService.BroadcastMessageAsync(requestStream.Current);
+            } while (await requestStream.MoveNext());
+
+            _chatroomService.Remove(context.Peer);
+        }
+
+        
+    }
+}
